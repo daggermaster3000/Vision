47741024ee0573b0a2c97d2404a67862121a5d3c
daggermaster3000 <quillan.favey@unil.ch>
2021-10-04 12:01:30 +0200

    bitasse

--- vision.py
+++ vision.py
@@ -10,42 +10,24 @@ import os
 import numpy as np
 import csv
 
-header = ['name','ratio']
+header = ['name','ratio','treat']
 log = []
-directory = r'C:\Users\quill\Downloads\frost data 2\iGEM\data-png'#input
-path = r'C:\Users\quill\OneDrive\Pictures\vision-images'#output
+directory = 'C:/Users/BIC-Mobility/Documents/Quillan/iGem/VISION/input-data4/crop'#input
+path = 'C:/Users/BIC-Mobility/Documents/Quillan/iGem/VISION/output-data4'#output
 
 #filter values
-lf_damage = [65, 0, 0]
-hf_damage = [179, 248, 87]
+lf_damage = [0, 75, 0]
+hf_damage = [179, 255, 255]
 lf_total = [0, 0, 0]
-hf_total = [179, 255, 152]
+hf_total = [254, 254, 254]
 
-
-def process_image(lf,uf):
-        print('Processing:',os.path.join(directory, filename)) 
-               
-        #Process image for blue damage
-        image = cv2.imread(os.path.join(directory, filename))
-        original = image.copy()
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower = np.array([65, 0, 0], dtype="uint8")
-        upper = np.array([179, 248, 87], dtype="uint8")
-        mask = cv2.inRange(image, lower, upper)
-     
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
-     
-        cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-     
-        damaged_area = 0
-        for c in cnts:
-         damaged_area += cv2.contourArea(c)
-         cv2.drawContours(original,[c], 0, (0,255,0), 2)
-     
-        print(damaged_area)
-     
+prefix_dic = {
+        "buffer":"buffer",
+        "Ffibp":"FFIBP",
+        "FfIBP":"FFIBP",
+        "FfiBP+RiAFP":"RI+FF-4",
+        "RiAFP":"RIAFP"
+        }
 
 
 for filename in os.listdir(directory):
@@ -53,12 +35,12 @@ for filename in os.listdir(directory):
     if filename.endswith(".jpg") or filename.endswith(".png"):
         print('Processing:',os.path.join(directory, filename)) 
                
-        #Process image for blue damage
+        #Process image for damaged area
         image = cv2.imread(os.path.join(directory, filename))
         original = image.copy()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower = np.array([65, 0, 0], dtype="uint8")
-        upper = np.array([179, 248, 87], dtype="uint8")
+        lower = np.array(lf_damage, dtype="uint8")
+        upper = np.array(hf_damage, dtype="uint8")
         mask = cv2.inRange(image, lower, upper)
      
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
@@ -89,8 +71,8 @@ for filename in os.listdir(directory):
         
         original_tot = image.copy()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower = np.array([0, 0, 0], dtype="uint8")
-        upper = np.array([179, 255, 152], dtype="uint8")
+        lower = np.array(lf_total, dtype="uint8")
+        upper = np.array(hf_total, dtype="uint8")
         mask_tot = cv2.inRange(image, lower, upper)
      
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
@@ -106,20 +88,29 @@ for filename in os.listdir(directory):
      
         print(area)
         
+
+
         #output processed images to output folder
+        
         size = len(filename)
         filename = filename[:size - 4]
         cv2.imwrite(os.path.join(path , filename+'_original.jpg'), original)
         cv2.imwrite(os.path.join(path , filename+'_mask.jpg'), mask)
         cv2.imwrite(os.path.join(path , filename+'_original-tot.jpg'), original_tot)
         cv2.imwrite(os.path.join(path , filename+'_mask-tot.jpg'), mask_tot)
+
+
+
         #write to log
-        log.append([filename,(np.round(damaged_area/area,2))*100])    
+        for i in prefix_dic.keys():
+            if filename.startswith(i):
+                log.append([filename,(np.round(damaged_area/area,2))*100,str(prefix_dic[i])])    
         
     else:
         continue
+
     #output log file
-with open(r'C:\Users\quill\OneDrive\Pictures\vision-images\data.csv', 'w', encoding='UTF8', newline='') as f:
+with open('C:/Users/BIC-Mobility/Documents/Quillan/iGem/VISION/output-data4/data.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
     # write the header
@@ -128,7 +119,4 @@ with open(r'C:\Users\quill\OneDrive\Pictures\vision-images\data.csv', 'w', encod
     # write multiple rows
     writer.writerows(log)
 
-    
-    
-    
     
